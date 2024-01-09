44d1eeb4e1f9c34ccf5fbf818f9d3b52d715d353
revillnik <revillnik@gmail.com>
2024-01-09 18:39:58 +0300

    next

--- firstd.py
+++ firstd.py
@@ -1,13 +1,15 @@
 import sqlite3
+
 with sqlite3.connect("first.bd") as base:
-	cursor = base.cursor()
-	cursor.execute("""CREATE TABLE IF NOT EXISTS first (
+    cursor = base.cursor()
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS first (
 					 id INTEGER PRIMARY KEY,
 					 name TEXT NOT NULL DEFAULT Вася,
 					 score INTEGER DEFAULT 0 
-	)""")
-	cursor.execute("SELECT * FROM first WHERE score > 500 ORDER BY score ASC LIMIT 2, 1")
-	result1 = cursor.fetchmany()
-	print(result1)
-	
-
+	)"""
+    )
+    cursor.execute("SELECT * FROM first WHERE score > 500 ORDER BY score ASC")
+    result1 = cursor.fetchall()
+    for i in result1:
+        print(i)
