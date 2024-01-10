25256dc9c66dbe9aa05f45de6417a4a750467ebc
revillnik <revillnik@gmail.com>
2024-01-10 20:33:01 +0300

    next

--- resquebd.py
+++ resquebd.py
@@ -5,6 +5,9 @@ with sqlite3.connect("car.sqlite3") as con:
 	with open('sql.sql', 'w') as f:
 		for i in con.iterdump():
 			f.write(i)
+	with open('sql.sql', 'r') as f:
+		sql = f.read()
+		cur.executescript(sql)
 
 		
         
