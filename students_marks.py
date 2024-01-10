73789066a6b676c01afcbe6a49561823db542641
revillnik <revillnik@gmail.com>
2024-01-10 16:46:41 +0300

    next

--- students_marks.py
+++ students_marks.py
@@ -26,3 +26,7 @@ with sqlite3.connect("st_mark.bd") as base:
                 discipline STR
 	)"""
     )
+    cursor.execute("INSERT INTO allinformation SELECT marks.id_student, students.name, marks.mark, marks.discipline FROM marks JOIN students ON students.id = marks.id_student")
+    cursor.execute("SELECT students.name, marks.mark, marks.discipline FROM marks JOIN students ON id_student = id WHERE marks.mark > (SELECT mark FROM marks WHERE id_student = 1 and discipline LIKE 'Си') AND discipline LIKE 'Си'")
+    f = cursor.fetchall()
+    print(f)
